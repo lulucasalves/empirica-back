f5c847c9a40f970de48820ba6bbebc7f908f3c05
lulucasalves <lucas.alves.supus@outlook.com>
2022-11-28 20:02:41 -0300

    refactor: minify functions

--- src/handlers/post_item.py
+++ src/handlers/post_item.py
@@ -5,8 +5,78 @@ import uuid
 import io
 import csv
 
-client = boto3.client('dynamodb')
+def changeColumnName(headerName, cell):
+    if (headerName == 'Originador'):
+        return {'headerName': 'ORIGINADOR', 'cell': cell, 'cellType': 'S'}
 
+    elif (headerName == 'Doc Originador'):
+        return {'headerName': 'DOC_ORIGINADOR', 'cell': formatDocument(cell), 'cellType': 'N'}
+
+    elif (headerName == 'Cedente'):
+        return {'headerName': 'CEDENTE', 'cell': cell, 'cellType': 'S'}
+
+    elif (headerName == 'Doc Cedente'):
+        return {'headerName': 'DOC_CEDENTE', 'cell': formatDocument(cell), 'cellType': 'S'}
+
+    elif (headerName == 'CCB'):
+        return {'headerName': 'CCB', 'cell': cell, 'cellType': 'N'}
+
+    elif (headerName == 'Id'):
+        return {'headerName': 'ID_EXTERNO', 'cell': cell, 'cellType': 'N'}
+
+    elif (headerName == 'Cliente'):
+        return {'headerName': 'CLIENTE', 'cell': cell, 'cellType': 'S'}
+
+    elif (headerName == 'CPF/CNPJ'):
+        return {'headerName': 'CPF_CNPJ', 'cell': formatDocument(cell), 'cellType': 'S'}
+
+    elif (headerName == 'Endereço'):
+        return {'headerName': 'ENDERECO', 'cell': cell, 'cellType': 'S'}
+
+    elif (headerName == 'Cidade'):
+        return {'headerName': 'CIDADE', 'cell': cell, 'cellType': 'S'}
+
+    elif (headerName == 'Valor do Empréstimo'):
+        return {'headerName': 'VALOR_DO_EMPRESTIMO', 'cell': formatFloat(cell), 'cellType': 'N'}
+
+    elif (headerName == 'Parcela R$'):
+        return {'headerName': 'VALOR_PARCELA', 'cell': formatFloat(cell), 'cellType': 'N'}
+
+    elif (headerName == 'Total Parcelas'):
+        return {'headerName': 'TOTAL_PARCELAS', 'cell': cell, 'cellType': 'N'}
+
+    elif (headerName == 'Parcela'):
+        return {'headerName': 'PARCELA', 'cell': cell, 'cellType': 'N'}
+
+    elif (headerName == 'Data de Emissão'):
+        return {'headerName': 'DATA_DE_EMISSAO', 'cell': formatDate(cell), 'cellType': 'S'}
+
+    elif (headerName == 'Data de Vencimento'):
+        return {'headerName': 'DATA_DE_VENCIMENTO', 'cell': formatDate(cell), 'cellType': 'S'}
+
+    elif (headerName == 'Preço de Aquisição'):
+        return {'headerName': 'PRECO_DE_AQUISICAO', 'cell': formatFloat(cell), 'cellType': 'N'}
+
+    elif (headerName == 'CEP' or headerName == 'UF'):
+        return {'headerName': headerName, 'cell': cell, 'cellType': 'S'}
+
+    else:
+        return {'headerName': '', 'cell': '', 'cellType': ''}
+
+
+def formatDocument(currentRow):
+    return currentRow.replace('.', '').replace('/', '').replace('-', '')
+
+
+def formatDate(currentRow):
+    day, month, year = currentRow.split('/')
+    return f'{year}-{month}-{day}'
+
+
+def formatFloat(currentRow):
+    return currentRow.replace(' ', '').replace(',', '.')
+
+client = boto3.client('dynamodb')
 
 def postItemHandler(event, context):
     if event["httpMethod"] != "POST":
@@ -17,93 +87,45 @@ def postItemHandler(event, context):
     name = body["bucket_name"]
     key = body["object_key"]
 
-    obj = boto3.client('s3').get_object(
+    s3Object = boto3.client('s3').get_object(
         Bucket=name, Key=key)
 
-    data = io.StringIO(obj['Body'].read().decode('ISO-8859-1'))
+    s3Data = io.StringIO(s3Object['Body'].read().decode('ISO-8859-1'))
 
-    my_reader = csv.reader(data)
+    s3DataReader = csv.reader(s3Data)
 
-    columns = []
-    num = 0
+    headersColumns = []
+    currentNumber = 0
 
-    def changeColumnName(current_name):
-        if (current_name == 'Originador'):
-            return 'ORIGINADOR'
-        elif (current_name == 'Doc Originador'):
-            return 'DOC_ORIGINADOR'
-        elif (current_name == 'Cedente'):
-            return 'CEDENTE'
-        elif (current_name == 'Doc Cedente'):
-            return 'DOC_CEDENTE'
-        elif (current_name == 'CCB'):
-            return 'CCB'
-        elif (current_name == 'Id'):
-            return 'ID_EXTERNO'
-        elif (current_name == 'Cliente'):
-            return 'CLIENTE'
-        elif (current_name == 'CPF/CNPJ'):
-            return 'CPF_CNPJ'
-        elif (current_name == 'Endereço'):
-            return 'ENDERECO'
-        elif (current_name == 'Cidade'):
-            return 'CIDADE'
-        elif (current_name == 'Valor do Empréstimo'):
-            return 'VALOR_DO_EMPRESTIMO'
-        elif (current_name == 'Parcela R$'):
-            return 'VALOR_PARCELA'
-        elif (current_name == 'Total Parcelas'):
-            return 'TOTAL_PARCELAS'
-        elif (current_name == 'Parcela'):
-            return 'PARCELA'
-        elif (current_name == 'Data de Emissão'):
-            return 'DATA_DE_EMISSAO'
-        elif (current_name == 'Data de Vencimento'):
-            return 'DATA_DE_VENCIMENTO'
-        elif (current_name == 'Preço de Aquisição'):
-            return 'PRECO_DE_AQUISICAO'
-        elif (current_name == 'CEP' or current_name == 'UF'):
-            return current_name
-        else:
-            return ''
-
-    def formatCpf(current_row):
-        return current_row.replace('.', '').replace('/', '').replace('-', '')
-
-    def formatDate(current_row):
-        day, month, year = current_row.split('/')
-        return f'{year}-{month}-{day}'
-
-    for row in my_reader:
-        x = str(row).replace("'", '').replace(
+    for row in s3DataReader:
+        rows = str(row).replace("'", '').replace(
             '[', '').replace(']', '').split(';')
-        numberof = len(x)-1
+        rowsLength = len(rows)-1
         id = uuid.uuid4()
 
-        for column in x:
-            if (num == 0):
-                columns.append(column)
+        for cell in rows:
+            if (currentNumber == 0):
+                headersColumns.append(cell)
 
-        objectList = [{'ID_SESSAO': {"S": str(id)}}]
+        objectsList = [{'ID_SESSAO': {"S": str(id)}}]
 
-        for i in range(numberof):
-            columnName = changeColumnName(columns[i])
-            if (len(columnName) and num != 0):
-                if (i == 1 or i == 7):
-                    objectList.append({columnName: {"S": formatCpf(x[i])}})
-                elif (i == 24 or i == 23):
-                    objectList.append({columnName: {"S": formatDate(x[i])}})
-                else:
-                    objectList.append({columnName: {"S": x[i]}})
+        for i in range(rowsLength):
+            if currentNumber != 0:
+                updatedData = changeColumnName(headersColumns[i], rows[i])
 
-                objects = {'ID_SESSAO': {"S": str(id)}}
+                if (len(updatedData['headerName'])):
+                    objectsList.append(
+                        {updatedData['headerName']: {updatedData['cellType']: updatedData['cell']}})
 
-                for o in objectList:
-                    objects.update(o)
+                    putItemObject = {'ID_SESSAO': {"S": str(id)}}
 
-                client.put_item(
-                    TableName=os.environ["SAMPLE_TABLE"], Item=objects)
-        num += 1
+                    for item in objectsList:
+                        putItemObject.update(item)
+
+                    client.put_item(
+                        TableName=os.environ["SAMPLE_TABLE"], Item=putItemObject)
+
+        currentNumber += 1
 
     response = {
         "statusCode": 200,
